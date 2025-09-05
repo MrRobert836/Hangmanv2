5c52ac08595658bb9b0109e06182621ac48fd6c2
MrRobert836 <shashkov.danila75@gmail.com>
2025-09-05 15:44:15 +0300

    Создан класс Gallows

---
+++ src/Gallows.java
@@ -0,0 +1,69 @@
+public class Gallows {
+
+    private static final String[][] PICTURES = {
+            {
+                    "-----   ",
+                    "|       ",
+                    "|       ",
+                    "|       ",
+                    "|       ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|       ",
+                    "|       ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|   |   ",
+                    "|       ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|  /|   ",
+                    "|       ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|  /|\\  ",
+                    "|       ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|  /|\\  ",
+                    "|  /    ",
+                    "------- "
+            },
+            {
+                    "-----   ",
+                    "|   |   ",
+                    "|   O   ",
+                    "|  /|\\  ",
+                    "|  / \\  ",
+                    "------- "
+            }
+    };
+
+    private static void printPicture(int numPicture){
+        String[] picture = PICTURES[numPicture];
+
+        for(String line: picture){
+            System.out.println(line);
+        }
+    }
+}
